d56cd8dd1855cfb910cf2cb8a629bde0e75d175f
Yuki IGARASHI <me@bonprosoft.com>
2015-02-19 01:47:08 +0900

    edit header,home,home.

--- footer.php
+++ footer.php
@@ -1,10 +1,34 @@
-		<!-- footer -->
-		<footer>
-			<div id="footeritem-container">
-				<p id="copyright" class="wrapper">&copy; Copyrights 2013<?php if (date("Y")!=2013) echo date("-Y"); ?> <?php bloginfo('name'); ?> All Rights Reserved.</p>
-			</div>
-		</footer>
-		<!-- /footer -->
-		<?php wp_footer(); ?>
-	</body>
+        </div>
+        <!-- /Main Container -->
+    </div>
+    <!-- /Main -->
+    <footer>
+        <div class="container">
+            <div class="box">
+                <div class="box__left">
+                  <div class="box">
+                    <div class="box__left">
+                      <img class="msplogo" src="img/logo.png" alt="">
+                    </div>
+                    <div class="box__right" style="margin:30px 0 0 20px;"> <!-- box in box -->
+                      <h3>このサイトについて</h3>
+                      <p>&copy; Copyrights 2013<?php if (date("Y")!=2013) echo date("-Y"); ?> <?php bloginfo('name'); ?> All Rights Reserved.</p>
+                      <p>このサイトはMSPメンバーが作り運営している<br>なんか凄いサイトです。</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="box__right">
+                    <div class="social-icons">
+                        <i class="fa fa-twitter fa-3x"></i>
+                        <i class="fa fa-facebook fa-3x"></i>
+                        <i class="fa fa-envelope fa-3x"></i>
+                    </div>
+                </div>
+            </div>
+            <p class="copyright">Copyright &copy; Microsoft Student Partners JAPAN All Right Reserved.</p>
+        </div>
+        <!-- /footer -->
+        <?php wp_footer(); ?>
+    </footer>
+</body>
 </html>
--- header.php
+++ header.php
@@ -1,43 +1,43 @@
 <!DOCTYPE html>
 <html lang="ja">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width,initial-scale=1" />
-		<!-- <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no" /> -->
-		<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.9.1/build/cssreset/cssreset-min.css">
-		<title><?php wp_title( '|', true, 'right' ); bloginfo('name'); ?></title>
-		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico">
-		<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" media="screen">
-		<script src="//code.jquery.com/jquery-latest.min.js"></script>
-		<script src="<?php echo get_template_directory_uri()."/js/jquery.flexslider.js"; ?>"></script>
-		<script src="<?php echo get_template_directory_uri()."/js/jquery.meanmenu.js"; ?>"></script>
-		<script src="<?php echo get_template_directory_uri()."/js/header.js"; ?>"></script>
-		<!--[if lt IE 9]>
-		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-		<![endif]-->
-		<?php if ( is_singular() ) wp_enqueue_script( "comment-reply" );
-		wp_head(); ?>
-	</head>
-	<body <?php body_class(); ?>>
-	<!-- Header -->
-	<header>
-			<div id="toplogo">
-				<a href="<?php echo home_url('/'); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" title="<?php bloginfo('name'); ?>" /></a>
-			</div>
-
-			<nav>
-				<?php wp_nav_menu( array ( 'theme_location' => 'header-navi' ) ); ?>
-
-				<div id="search-menu">
-					<?php get_search_form(); ?>
-				</div>
-			</nav>
-
-			<div id="menubtn"></div>
-
-		</div>
-	</header>
-	<!-- /Header -->
-
-	<div id="maincontents">
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title><?php wp_title( '|', true, 'right' ); bloginfo('name'); ?></title>
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico">
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" media="screen">
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <?php if ( is_singular() ) wp_enqueue_script( "comment-reply" );
+        wp_head(); ?>
+    </head>
+</head>
+<body <?php body_class(); ?>>
+    <!-- Header -->
+    <header>
+        <div class="container">
+            <div class="box">
+                <div class="box__left">
+                    <h1 class="title">Microsoft Student Partners JAPAN</h1>
+                </div>
+                <div class="box__right">
+                    <!-- <img class="menu" src="img/menu.png" alt=""> -->
+                    <div class="navbar">
+                      <div class="navbar_box"><a href="#">Top</a></div>
+                      <div class="navbar_box"><a href="#">About</a></div>
+                      <div class="navbar_box"><a href="#">Info</a></div>
+                      <div class="navbar_box"><a href="#">Profile</a></div>
+                      <div class="navbar_box"><a href="#">Project</a></div>
+                      <div class="navbar_box"><a href="#">Blog</a></div>
+                      <div class="navbar_box"><a href="#">Contact</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- /Header -->
+    <!-- Main -->
+    <div class="main">
+        <!-- Main Container -->
+        <div class="container">
