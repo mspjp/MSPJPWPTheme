52a57716a9db2a7628d64ff827a016f970eedc21
Yuki IGARASHI <me@bonprosoft.com>
2015-02-19 03:15:43 +0900

    edit single.php

--- single.php
+++ single.php
@@ -1,85 +1,81 @@
 <?php get_header(); ?>
 <?php wp_link_pages(); ?>
-			<!-- main -->
-			<div id="main">
-				<div id="leftcol">
-				<?php 
-				if (have_posts()) : // WordPress ループ
-					while (have_posts()) : the_post(); // 繰り返し処理開始 ?>
-						<article class="entry-container" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-							<h2>
-								<p><?php echo get_the_date(); ?></p>
-								<p><a href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a></p>
-							</h2>
-							<div class="post-description"><?php echo get_post_meta($post->ID , 'description' ,true); ?></div>
-							<div class="post-meta">
-								<p class="post-date"><?php echo get_the_date(); ?></p>
-								<div class="category"><ul><li><?php the_category('</li><li>') ?></li></ul></div>
-								<p class="comment-num"><?php comments_popup_link('Comment : 0', 'Comment : 1', 'Comments : %'); ?></p>
-							</div>
+            <!-- Left Col -->
+            <div id="leftcol">
+            <?php
+            if (have_posts()) : // WordPress ループ
+                while (have_posts()) : the_post(); // 繰り返し処理開始 ?>
+                <article class="entry-container" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <div class="post_date"><?php echo get_the_date(); ?></div>
+                    <div class="post_title">
+                      <h1><a href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a></h1>
+                    </div>
+                    <div class="post_meta">
+                      <div class="post_category">
+                        <ul>
+                            <li><?php the_category('</li><li>') ?></li>
+                        </ul>
+                      </div>
+                     <!-- メタデータを追加する場合はここ -->
+                       <div class="comment_num">
+                         <p><?php comments_popup_link('Comment : 0', 'Comment : 1', 'Comments : %'); ?></p>
+                       </div>
+                    </div>
 
-							<?php if(function_exists("wp_social_bookmarking_light_output_e")){wp_social_bookmarking_light_output_e(null, get_permalink(), the_title("", "", false));}?>
+                    <div class="entry_body">
+                    <?php the_content(); ?>
+                    </div>
+                    <?php if(function_exists("wp_social_bookmarking_light_output_e")){wp_social_bookmarking_light_output_e(null, get_permalink(), the_title("", "", false));}?>
 
-							<div class="entrybody">
-								<?php the_content(); ?>
-							</div>
-							
-							<?php 
-							$args = array(
-								'before' => '<div class="page-link">',
-								'after' => '</div>',
-								'link_before' => '<span>',
-								'link_after' => '</span>',
-							);
-							wp_link_pages($args); ?>
+                    <?php
+                    $args = array(
+                        'before' => '<div class="page-link">',
+                        'after' => '</div>',
+                        'link_before' => '<span>',
+                        'link_after' => '</span>',
+                    );
+                    wp_link_pages($args); ?>
 
-							<?php if(function_exists("wp_social_bookmarking_light_output_e")){wp_social_bookmarking_light_output_e(null, get_permalink(), the_title("", "", false));}?>
+                    <?php if(function_exists("wp_social_bookmarking_light_output_e")){wp_social_bookmarking_light_output_e(null, get_permalink(), the_title("", "", false));}?>
 
-							<p class="footer-post-meta">
-								<?php the_tags('Tag : ',', '); ?>
-								<?php if ( is_multi_author() ): ?> 
-								<span class="post-author">作成者 : <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php the_author(); ?></a></span>
-								<?php
-								endif;
-								?>
-							</p>
+                    <p class="footer-post-meta">
+                        <?php the_tags('Tag : ',', '); ?>
 
-						</article>
+                        <?php if ( is_multi_author() ): ?> 
+                        <span class="post-author">作成者 : <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php the_author(); ?></a></span>
+                        <?php
+                        endif;
+                        ?>
+                    </p>
+                </article>
 
-						
-						<!-- post navigation -->
-						<div class="navigation">
-							<?php 
-							if( get_previous_post() ): ?>
-								<div class="alignleft"><?php previous_post_link('%link', '&laquo; %title'); ?></div>
-							<?php 
-							endif;
-							
-							if( get_next_post() ): ?>
-								<div class="alignright"><?php next_post_link('%link', '%title &raquo;'); ?></div>
-							<?php 
-							endif; 
-							?>
-						</div>
-						<!-- /post navigation -->
-						<?php comments_template(); // コメント欄の表示 ?>
-						
-					<?php 
-					endwhile; // 繰り返し処理終了		
-				else : // ここから記事が見つからなかった場合の処理 ?>
-						<div class="post">
-							<h2>記事はありません</h2>
-							<p>お探しの記事は見つかりませんでした。</p>
-						</div>
-				<?php
-				endif;
-				?>
-				</div><!-- /leftcol -->
-			<?php get_sidebar(); ?>
-	
-			</div>
-			<!-- /main -->
-		</div>
-		<!-- /maincontents -->
+                <!-- post navigation -->
+                <div class="navigation">
+                    <?php
+                    if( get_previous_post() ): ?>
+                    <div class="alignleft"><?php previous_post_link('%link', '&laquo; %title'); ?></div>
+                    <?php
+                    endif;
+                    if( get_next_post() ): ?>
+                    <div class="alignright"><?php next_post_link('%link', '%title &raquo;'); ?></div>
+                    <?php
+                        endif;
+                    ?>
+                </div>
+                <!-- /post navigation -->
+                <?php comments_template(); // コメント欄の表示 ?>
+                <?php
+                endwhile; // 繰り返し処理終了
+                else : // ここから記事が見つからなかった場合の処理 ?>
+                <div class="post">
+                    <h2>記事はありません</h2>
+                    <p>お探しの記事は見つかりませんでした。</p>
+                </div>
+                <?php
+                endif;
+                ?>
+            </div>
+            <!-- /Leftcol -->
+            <?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
