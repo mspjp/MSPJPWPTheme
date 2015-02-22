45ca762f4c133f612dc67d93d9ce1d1393c2f24a
Yuki IGARASHI <me@bonprosoft.com>
2015-02-22 19:43:37 +0900

    update archive-profile

--- archive-profile.php
+++ archive-profile.php
@@ -1,40 +1,38 @@
-<?php get_header(); ?>
-<?php wp_link_pages(); ?>
-            <!-- Left Col -->
+<?php get_header();
+$post_type = $wp_query->get_queried_object();
+?>
             <div class="headline">
-                <h1 class="headline_title">メンバー</h1>
-                <?php if(function_exists("wp_social_bookmarking_light_output_e")){wp_social_bookmarking_light_output_e(null, get_permalink(), post_type_archive_title());}?>
+                <h2 class="headline_title"><?php post_type_archive_title(); ?></h2>
+                <div class="headline__snsbtns">
+                    <?php if(function_exists("wp_social_bookmarking_light_output_e")){wp_social_bookmarking_light_output_e(null, get_post_type_archive_link(), $post_type->labels->name );}?>
+                </div>
             </div>
-            <div class="profile_base">
-                <ul>
+
+            <!-- Main Content -->
+            <ul class="profile_list">
                 <?php
-                query_posts( $query_string . "&posts_per_page=8&paged=".$paged );
+                query_posts( $query_string . "&posts_per_page=5&paged=".$paged );
                 if (have_posts()) : // WordPress ループ
                     while (have_posts()) : the_post(); // 繰り返し処理開始 ?>
-                    <li>
-                    <a class="profile_box" href="<?php the_permalink(); ?>">
-                        <?php echo get_the_post_thumbnail($page->ID, 'thumbnail'); ?> <!-- img  -->
-                        <h1 class="profile_header"><?php echo get_the_title(); ?></h1>
-                        <div class="profile_content">
-                            <?php the_content(); ?>
-                        </div>
+                <li id="post-<?php the_ID(); ?>">
+                    <a class="profile" href="<?php the_permalink(); ?>">
+                        <img class="profile__icon" src="<?php if( has_post_thumbnail() ){echo get_thumbnail_url();}else{echo get_template_directory_uri()."/img/noimage.png";} ?>" alt="">
+                        <h3 class="profile__name"><?php echo mb_substr(get_the_title(), 0, 30); ?></h3>
+                        <p class="profile__text"><?php echo mb_substr(strip_tags($post-> post_content), 0, 60)."..."; ?></p>
                     </a>
-                    </li>
-
-                    <!-- /post navigation -->
-                    <?php
-                    endwhile; // 繰り返し処理終了
+                </li>
+                <?php
+                endwhile; // 繰り返し処理終了
                 else : // ここから記事が見つからなかった場合の処理 ?>
-                    <div class="post">
-                        <h2>記事はありません</h2>
-                        <p>お探しの記事は見つかりませんでした。</p>
-                    </div>
+                <div class="post">
+                    <h2>アーカイブはありません</h2>
+                    <p>お探しのアーカイブは見つかりませんでした。</p>
+                </div>
                 <?php
                 endif;
                 ?>
-                </ul>
-
-            <!-- page navigation -->
+            </div>
+            <!-- /Main Content -->
             <?php
             if ( $wp_query -> max_num_pages > 1 ) : ?>
             <div class="navigation">
@@ -44,7 +42,5 @@
             <?php 
             endif;
             ?>
-
-        </div>
-        <!-- Profile Base -->
+            <?php get_sidebar(); ?>
 <?php get_footer(); ?>
