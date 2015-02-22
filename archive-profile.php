482bfd981f3e0890c05167515e27546707bb386d
Yuki IGARASHI <me@bonprosoft.com>
2015-02-22 12:24:53 +0900

    expand tab.

--- archive-profile.php
+++ archive-profile.php
@@ -6,51 +6,45 @@
                 <?php if(function_exists("wp_social_bookmarking_light_output_e")){wp_social_bookmarking_light_output_e(null, get_permalink(), the_title("", "", false));}?>
             </div>
             <div class="profile_base">
-	            <ul>
-	            <?php
-	            query_posts( $query_string . "&posts_per_page=8&paged=".$paged );
-	            if (have_posts()) : // WordPress ループ
-	                while (have_posts()) : the_post(); // 繰り返し処理開始 ?>
-	                <li>
-	                <a class="profile_box" href="<?php the_permalink(); ?>">
-	                	<?php echo get_the_post_thumbnail($page->ID, 'thumbnail'); ?> <!-- img  -->
-	                	<h1 class="profile_header"><?php echo get_the_title(); ?></h1>
-	                	<div class="profile_content">
-	                		<?php the_content(); ?>
-	                	</div>
-	                </a>
-	                </li>
-	    
-	                <!-- /post navigation -->
-	                <?php
-	                endwhile; // 繰り返し処理終了
-	                else : // ここから記事が見つからなかった場合の処理 ?>
-	                <div class="post">
-	                    <h2>記事はありません</h2>
-	                    <p>お探しの記事は見つかりませんでした。</p>
-	                </div>
-	                <?php
-	                endif;
-	                ?>
-	            </ul>
-	            
-	            <!-- page navigation -->
-	            <?php
-                if ( $wp_query -> max_num_pages > 1 ) : ?>
-                <div class="navigation">
-                    <div class="alignleft"><?php next_posts_link('&laquo; PREV'); ?></div>
-                    <div class="alignright"><?php previous_posts_link('NEXT &raquo;'); ?></div>
-                </div>
-                <?php 
+                <ul>
+                <?php
+                query_posts( $query_string . "&posts_per_page=8&paged=".$paged );
+                if (have_posts()) : // WordPress ループ
+                    while (have_posts()) : the_post(); // 繰り返し処理開始 ?>
+                    <li>
+                    <a class="profile_box" href="<?php the_permalink(); ?>">
+                        <?php echo get_the_post_thumbnail($page->ID, 'thumbnail'); ?> <!-- img  -->
+                        <h1 class="profile_header"><?php echo get_the_title(); ?></h1>
+                        <div class="profile_content">
+                            <?php the_content(); ?>
+                        </div>
+                    </a>
+                    </li>
+
+                    <!-- /post navigation -->
+                    <?php
+                    endwhile; // 繰り返し処理終了
+                else : // ここから記事が見つからなかった場合の処理 ?>
+                    <div class="post">
+                        <h2>記事はありません</h2>
+                        <p>お探しの記事は見つかりませんでした。</p>
+                    </div>
+                <?php
                 endif;
                 ?>
-	            
+                </ul>
+
+            <!-- page navigation -->
+            <?php
+            if ( $wp_query -> max_num_pages > 1 ) : ?>
+            <div class="navigation">
+                <div class="alignleft"><?php next_posts_link('&laquo; PREV'); ?></div>
+                <div class="alignright"><?php previous_posts_link('NEXT &raquo;'); ?></div>
             </div>
-            <!-- Box Left -->
+            <?php 
+            endif;
+            ?>
 
-
-            </div>
-            <!-- /Leftcol -->
-
-
+        </div>
+        <!-- Profile Base -->
 <?php get_footer(); ?>
--- page.php
+++ page.php
@@ -1,39 +1,39 @@
 <?php get_header(); ?>
-			<!-- main -->
-			<div id="main">
-				<div class="headline">
+            <!-- main -->
+            <div id="main">
+                <div class="headline">
                     <h1 class="headline_title"><?php echo get_the_title(); ?></h1>
-					<?php if(function_exists("wp_social_bookmarking_light_output_e")){wp_social_bookmarking_light_output_e(null, get_permalink(), the_title("", "", false));}?>
+                    <?php if(function_exists("wp_social_bookmarking_light_output_e")){wp_social_bookmarking_light_output_e(null, get_permalink(), the_title("", "", false));}?>
                 </div>
-				<?php 
-				if (have_posts()) : // WordPress ループ
-					while (have_posts()) : the_post(); // 繰り返し処理開始 ?>
-						<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-							
-							<!-- ここに謎に表示されるソーシャルボタンたちを消したい -->
-							
-							<?php the_content(); ?>
-							
-							<?php 
-							$args = array(
-								'before' => '<div class="page-link">',
-								'after' => '</div>',
-								'link_before' => '<span>',
-								'link_after' => '</span>',
-							);
-							wp_link_pages($args); ?>
-							
-						</div>
-					<?php 
-					endwhile; // 繰り返し処理終了		
-				else : // ここから記事が見つからなかった場合の処理 ?>
-						<div class="post">
-							<h2>記事はありません</h2>
-							<p>お探しの記事は見つかりませんでした。</p>
-						</div>
-				<?php
-				endif;
-				?>
-			</div>
-			<!-- /main -->
+            <?php 
+            if (have_posts()) : // WordPress ループ
+                while (have_posts()) : the_post(); // 繰り返し処理開始 ?>
+                    <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+                    <!-- ここに謎に表示されるソーシャルボタンたちを消したい -->
+
+                    <?php the_content(); ?>
+
+                    <?php 
+                    $args = array(
+                        'before' => '<div class="page-link">',
+                        'after' => '</div>',
+                        'link_before' => '<span>',
+                        'link_after' => '</span>',
+                    );
+                    wp_link_pages($args); ?>
+
+                    </div>
+                <?php 
+                endwhile; // 繰り返し処理終了		
+                else : // ここから記事が見つからなかった場合の処理 ?>
+                <div class="post">
+                    <h2>記事はありません</h2>
+                    <p>お探しの記事は見つかりませんでした。</p>
+                </div>
+                <?php
+                endif;
+                ?>
+            </div>
+            <!-- /main -->
 <?php get_footer(); ?>
