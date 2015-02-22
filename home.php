c28f6d423f5f60d6f6c2a22f16905823908dd69b
Yuki IGARASHI <me@bonprosoft.com>
2015-02-22 23:00:05 +0900

    fix absolute url.

--- home.php
+++ home.php
@@ -95,7 +95,7 @@
             <!-- /Top Info -->
             <script src="http://code.jquery.com/jquery-1.9.0.js"></script>
             <script src="http://code.jquery.com/jquery-migrate-1.1.0.js"></script>
-            <script type="text/javascript" src="slick/slick/slick.min.js"></script>
+            <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/css/slick/slick.min.js"></script>
             <script>
             $(document).ready(function(){
               $('.topslider').slick({
