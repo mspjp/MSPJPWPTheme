5ce0d4f1b0ddf07787af788d6a6a4c3003e96278
Yuki IGARASHI <me@bonprosoft.com>
2015-02-25 09:42:48 +0900

    update page.php.

--- page.php
+++ page.php
@@ -1,31 +1,57 @@
 <?php get_header(); ?>
-            <!-- main -->
-            <div id="main">
-                <div class="headline">
-                    <h1 class="headline_title"><?php echo get_the_title(); ?></h1>
-                    <?php if(function_exists("wp_social_bookmarking_light_output_e")){wp_social_bookmarking_light_output_e(null, get_permalink(), the_title("", "", false));}?>
-                </div>
-            <?php 
-            if (have_posts()) : // WordPress ループ
-                while (have_posts()) : the_post(); // 繰り返し処理開始 ?>
-                    <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<?php wp_link_pages(); ?>
+            <div class="main__content">
+                <!-- Report -->
+                <div id="post-<?php the_ID(); ?>" <?php post_class("report"); ?>>
+                    <?php
+                    if (have_posts()) : // WordPress ループ
+                        while (have_posts()) : the_post(); // 繰り返し処理開始 ?>
+                    <div class="report__meta">
+                        <div class="post_title">
+                            <h3 class="report__title"><a href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a></h3>
+                        </div>
+                        <ul class="report__category">
+                            <?php wp_list_categories(); ?>
+                        </ul>
+                        <?php if ( comments_open() ) : ?>
+                            <p><?php comments_popup_link('Comment : 0', 'Comment : 1', 'Comments : %'); ?></p>
+                        <?php endif; // comments_open() ?>
 
-                    <!-- ここに謎に表示されるソーシャルボタンたちを消したい -->
-
-                    <?php the_content(); ?>
-
-                    <?php 
-                    $args = array(
-                        'before' => '<div class="page-link">',
-                        'after' => '</div>',
-                        'link_before' => '<span>',
-                        'link_after' => '</span>',
-                    );
-                    wp_link_pages($args); ?>
-
+                        <p class="report__info">by <?php the_author(); ?> | <?php echo get_the_date(); ?></p>
                     </div>
-                <?php 
-                endwhile; // 繰り返し処理終了		
+
+                    <div class="entry_body">
+                    <?php the_content(); ?>
+                    </div>
+
+                </div>
+                <?php if(function_exists("wp_social_bookmarking_light_output_e")){wp_social_bookmarking_light_output_e(null, get_permalink(), the_title("", "", false));}?>
+                <?php
+                $args = array(
+                    'before' => '<div class="page-link">',
+                    'after' => '</div>',
+                    'link_before' => '<span>',
+                    'link_after' => '</span>',
+                );
+                wp_link_pages($args); ?>
+
+                <!-- post navigation -->
+                <div class="navigation">
+                    <?php
+                    if( get_previous_post() ): ?>
+                    <div class="alignleft"><?php previous_post_link('%link', '&laquo; %title'); ?></div>
+                    <?php
+                    endif;
+                    if( get_next_post() ): ?>
+                    <div class="alignright"><?php next_post_link('%link', '%title &raquo;'); ?></div>
+                    <?php
+                        endif;
+                    ?>
+                </div>
+                <!-- /post navigation -->
+                <?php comments_template(); // コメント欄の表示 ?>
+                <?php
+                endwhile; // 繰り返し処理終了
                 else : // ここから記事が見つからなかった場合の処理 ?>
                 <div class="post">
                     <h2>記事はありません</h2>
@@ -34,6 +60,11 @@
                 <?php
                 endif;
                 ?>
+                </div>
+                <!-- /Report -->
+                <div class="main__sidebar">
+                    <?php get_sidebar(); ?>
+                </div>
             </div>
-            <!-- /main -->
+            <!-- /Main Content -->
 <?php get_footer(); ?>
--- single-profile.php
+++ single-profile.php
@@ -1,86 +1,70 @@
 <?php get_header(); ?>
 <?php wp_link_pages(); ?>
-            <!-- Left Col -->
-            <div class="box">
-                <div class="box__left column__left">
-                    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <div class="main__content">
+                <!-- Report -->
+                <div id="post-<?php the_ID(); ?>" <?php post_class("report"); ?>>
                     <?php
                     if (have_posts()) : // WordPress ループ
                         while (have_posts()) : the_post(); // 繰り返し処理開始 ?>
-                        <div class="post_date"><?php echo get_the_date(); ?></div>
+                    <div class="report__meta">
                         <div class="post_title">
-                            <h1><a href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a></h1>
+                            <h3 class="report__title"><a href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a></h3>
                         </div>
-                        <div class="post_meta">
-                            <div class="post_category">
-                                <ul>
-                                    <li><?php the_category('</li><li>') ?></li>
-                                </ul>
-                            </div>
-                            <!-- メタデータを追加する場合はここ -->
-                            <div class="comment_num">
-                                <p><?php comments_popup_link('Comment : 0', 'Comment : 1', 'Comments : %'); ?></p>
-                            </div>
-                        </div>
+                        <ul class="report__category">
+                            <?php wp_list_categories(); ?>
+                        </ul>
+                        <?php if ( comments_open() ) : ?>
+                            <p><?php comments_popup_link('Comment : 0', 'Comment : 1', 'Comments : %'); ?></p>
+                        <?php endif; // comments_open() ?>
 
-                        <div class="entry_body">
-                        <?php the_content(); ?>
-                        </div>
-
-                        <div class="entry_auther">
-                            <p>auther: <?php the_author(); ?></p>
-                        </div>
-
-                        <p class="footer-post-meta">
-                            <?php the_tags('Tag : ',', '); ?>
-
-                            <?php if ( is_multi_author() ): ?> 
-                            <span class="post-author">作成者 : <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php the_author(); ?></a></span>
-                            <?php
-                            endif;
-                            ?>
-                        </p>
-                    </article>
-                    <?php if(function_exists("wp_social_bookmarking_light_output_e")){wp_social_bookmarking_light_output_e(null, get_permalink(), the_title("", "", false));}?>
-                    <?php
-                    $args = array(
-                        'before' => '<div class="page-link">',
-                        'after' => '</div>',
-                        'link_before' => '<span>',
-                        'link_after' => '</span>',
-                    );
-                    wp_link_pages($args); ?>
-
-                    <!-- post navigation -->
-                    <div class="navigation">
-                        <?php
-                        if( get_previous_post() ): ?>
-                        <div class="alignleft"><?php previous_post_link('%link', '&laquo; %title'); ?></div>
-                        <?php
-                        endif;
-                        if( get_next_post() ): ?>
-                        <div class="alignright"><?php next_post_link('%link', '%title &raquo;'); ?></div>
-                        <?php
-                            endif;
-                        ?>
+                        <p class="report__info">by <?php the_author(); ?> | <?php echo get_the_date(); ?></p>
                     </div>
-                    <!-- /post navigation -->
-                    <?php comments_template(); // コメント欄の表示 ?>
-                    <?php
-                    endwhile; // 繰り返し処理終了
-                    else : // ここから記事が見つからなかった場合の処理 ?>
-                    <div class="post">
-                        <h2>記事はありません</h2>
-                        <p>お探しの記事は見つかりませんでした。</p>
+
+                    <div class="entry_body">
+                    <?php the_content(); ?>
                     </div>
+
+                </div>
+                <?php if(function_exists("wp_social_bookmarking_light_output_e")){wp_social_bookmarking_light_output_e(null, get_permalink(), the_title("", "", false));}?>
+                <?php
+                $args = array(
+                    'before' => '<div class="page-link">',
+                    'after' => '</div>',
+                    'link_before' => '<span>',
+                    'link_after' => '</span>',
+                );
+                wp_link_pages($args); ?>
+
+                <!-- post navigation -->
+                <div class="navigation">
+                    <?php
+                    if( get_previous_post() ): ?>
+                    <div class="alignleft"><?php previous_post_link('%link', '&laquo; %title'); ?></div>
                     <?php
                     endif;
+                    if( get_next_post() ): ?>
+                    <div class="alignright"><?php next_post_link('%link', '%title &raquo;'); ?></div>
+                    <?php
+                        endif;
                     ?>
                 </div>
-                <!-- /Box Left -->
-                <div class="box__right column__right">
+                <!-- /post navigation -->
+                <?php comments_template(); // コメント欄の表示 ?>
+                <?php
+                endwhile; // 繰り返し処理終了
+                else : // ここから記事が見つからなかった場合の処理 ?>
+                <div class="post">
+                    <h2>記事はありません</h2>
+                    <p>お探しの記事は見つかりませんでした。</p>
+                </div>
+                <?php
+                endif;
+                ?>
+                </div>
+                <!-- /Report -->
+                <div class="main__sidebar">
                     <?php get_sidebar(); ?>
                 </div>
             </div>
-            <!-- /Box -->
+            <!-- /Main Content -->
 <?php get_footer(); ?>
