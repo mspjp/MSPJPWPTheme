26e6924531b8c7abb5740104bbaa09a6f05a248d
damjing <mjoy.bluesky@gmail.com>
2015-02-27 17:58:46 +0900

    test

--- single-profile.php
+++ single-profile.php
@@ -15,7 +15,7 @@
                     </ul>
                     <p><?php the_meta(); ?></p>
                     <p class="profilepage__year"><a href="">所属年度：2014年度</a></p>
-<?php 
+<!-- <?php 
  $custom_fields = get_post_custom();
 
  $teams = $custom_fields['team'];
@@ -47,8 +47,57 @@
 
 
 
+?> -->
+
+
+<!-- test here -->
+<?php
+
+/*
+*  Loop through post objects (assuming this is a multi-select field) ( setup postdata )
+*  Using this method, you can use all the normal WP functions as the $post object is temporarily initialized within the loop
+*  Read more: http://codex.wordpress.org/Template_Tags/get_posts#Reset_after_Postlists_with_offset
+*/
+
+$post_objects = get_field('post_objects');
+
+if( $post_objects ): ?>
+    <ul>
+    <?php foreach( $post_objects as $post): // variable must be called $post (IMPORTANT) ?>
+        <?php setup_postdata($post); ?>
+        <li>
+            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            <span>Post Object Custom Field: <?php the_field('field_name'); ?></span>
+        </li>
+    <?php endforeach; ?>
+    </ul>
+    <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+<?php endif;
+
+/*
+*  Loop through post objects (assuming this is a multi-select field) ( don't setup postdata )
+*  Using this method, the $post object is never changed so all functions need a seccond parameter of the post ID in question.
+*/
+
+$post_objects = get_field('post_objects');
+
+if( $post_objects ): ?>
+    <ul>
+    <?php foreach( $post_objects as $post_object): ?>
+        <li>
+            <a href="<?php echo get_permalink($post_object->ID); ?>"><?php echo get_the_title($post_object->ID); ?></a>
+            <span>Post Object Custom Field: <?php the_field('field_name', $post_object->ID); ?></span>
+        </li>
+    <?php endforeach; ?>
+    </ul>
+<?php endif;
+
 ?>
-					<a href="<?php the_field('page_link'); ?>">Read this!</a>
+
+<!-- test end -->
+
+
+
                     <p class="profilepage__username"><a href="#">ユーザ名：@admin_admin</a></p>
                     <p>所属プロジェクト：</p>
                     <ul class="profilepage__projects">
