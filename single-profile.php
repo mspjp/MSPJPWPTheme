2f135225977627f7c3c9b37ece083003dc061435
damjing <mjoy.bluesky@gmail.com>
2015-03-03 15:56:00 +0900

    test

--- single-profile.php
+++ single-profile.php
@@ -22,13 +22,10 @@
 							<?php
 						    $teams = get_field("team");
 						    foreach ($teams as $teamkey) : ?>
-						        <li><a href="<?php echo $permalink = get_permalink( echo $teamkey ); ?>"> ちーむ名を出したい </a></li>
+						        <li><a href="<?php echo $permalink = get_permalink( echo $teamkey ); ?>"><?php echo $teamkey; ?></a></li>
 						    <?php endforeach; ?>
 						</ul>
 					<?php endif; ?>
-					
-					
-					
 					
 					
 <!-- <?php 
