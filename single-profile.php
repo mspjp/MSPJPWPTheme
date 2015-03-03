2f31680b6de79d09e6bc4ec5113747f9aca560df
damjing <mjoy.bluesky@gmail.com>
2015-03-03 16:59:40 +0900

    test

--- single-profile.php
+++ single-profile.php
@@ -20,13 +20,13 @@
 
 					$teams = get_field('team');
 					if($teams)
-					{</li>
+					{
 						echo '<ul>';
 
 						foreach($teams as $teamid)
-						{ ?>
-							<li><a href="<?php $permalink = get_permalink( echo $teamid ); ?>"><?php echo $teamid; ?></a></li>
-						<?php}
+						{
+							echo '<li>' . $teamid . '</li>';
+						}
 
 						echo '</ul>';
 					}
