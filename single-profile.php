f4bc896699394086607d02781d6bc88cb29cc44b
damjing <mjoy.bluesky@gmail.com>
2015-03-03 15:01:03 +0900

    test

--- single-profile.php
+++ single-profile.php
@@ -15,7 +15,7 @@
                     </ul>
                     <p><?php the_meta(); ?></p>
                     <p class="profilepage__year"><a href="">所属年度：2014年度</a></p>
-                    <?php the_field('team', '35'); ?>
+                    <?php the_field("team", $post->ID); ?>
 <!-- <?php 
  $custom_fields = get_post_custom();
 
