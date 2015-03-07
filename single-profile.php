a3448501f9aaab3ef3ddb700da9944ac918cbfd0
damjing <mjoy.bluesky@gmail.com>
2015-03-08 00:42:10 +0900

    test

--- single-profile.php
+++ single-profile.php
@@ -38,7 +38,8 @@
 
 							foreach($teams as $teamid)
 							{
-								echo '<li><a href="' . get_permalink( 35 ) . '">' . $teamid . '</a></li>';
+								$temtem = $teamid
+								echo '<li><a href="' . get_permalink( $temtem ) . '">' . $teamid . '</a></li>';
 							}
 
 							echo '</ul>';
