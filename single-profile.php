ff50de89ba5eb760539ec1fd3e1bac393163a430
damjing <mjoy.bluesky@gmail.com>
2015-03-13 19:30:33 +0900

    change limit words

--- single-profile.php
+++ single-profile.php
@@ -63,8 +63,8 @@
 								?>
                             </div>
                             <div class="post__content">
-                                <h3 class="post__title"><?php echo mb_substr(get_the_title($article->ID), 0, 100)."..."; ?></h3>
-                                <p class="post__text"><?php echo mb_substr(strip_tags($article->post_content), 0, 100)."..."; ?></p>
+                                <h3 class="post__title"><?php echo mb_substr(get_the_title($article->ID), 0, 100); ?></h3>
+                                <p class="post__text"><?php echo mb_substr(strip_tags($article->post_content), 0, 100); ?></p>
                                 <p class="post__info">by <?php the_author(); ?> | <?php the_time('Y年n月j日') ;?> | <?php comments_popup_link('Comment : 0', 'Comment : 1', 'Comments : %'); ?></p>
                             </div>
                     </a>
