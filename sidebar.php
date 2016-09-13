6430314125f027e856597eae924f399546b4d8d9
garicchi <xgaricchi@gmail.com>
2016-09-14 07:32:02 +0900

    add feedly button

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,6 @@
+<!--feedly button-->
+<a href='http://cloud.feedly.com/#subscription%2Ffeed%2Fhttp%3A%2F%2Fmspjp.net%2Ffeed%2Frss2'  target='blank'><img id='feedlyFollow' src='http://s3.feedly.com/img/follows/feedly-follow-rectangle-flat-big_2x.png' alt='follow us in feedly' width='131' height='56'></a>
+
 <div class="twitter" style="max-width:100%">
 <a class="twitter-timeline" href="https://twitter.com/_mspjp" data-widget-id="690779412665540608">Tweets by @_mspjp</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 </div>
