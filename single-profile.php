1161ce68b4f97798c20e95d19f88b64670b5d61e
garicchi <xgaricchi@gmail.com>
2016-11-03 11:59:47 +0900

    remove navigation

--- single-profile.php
+++ single-profile.php
@@ -95,33 +95,6 @@
                     </div>
                 </div>
                 
-                <!--
-                <?php
-                $args = array(
-                    'before' => '<div class="page-link">',
-                    'after' => '</div>',
-                    'link_before' => '<span>',
-                    'link_after' => '</span>',
-                );
-                wp_link_pages($args); 
-                
-                ?>
-
-                <div class="navigation">
-                    <?php
-                    if( get_previous_post() ): ?>
-                    <div class="align-left"><?php previous_post_link('%link', '&laquo; %title'); ?></div>
-                    <?php
-                    endif;
-                    if( get_next_post() ): ?>
-                    <div class="align-right"><?php next_post_link('%link', '%title &raquo;'); ?></div>
-                    <?php
-                        endif;
-                    ?>
-                </div>
-                -->
-                <!-- /post navigation -->
-                <?php comments_template(); // コメント欄の表示 ?>
                 <?php
                 endwhile; // 繰り返し処理終了
                 else : // ここから記事が見つからなかった場合の処理 ?>
